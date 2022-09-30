6ebf3156e38fdf644b1100680684616f66b032ee
Joris Vos <jorisvos2000@gmail.com>
2022-09-30 17:31:47 +0200

    🧑‍💻setup project

---
+++ spigot-wrapper-gui/src/core/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme, ThemeOptions } from '@mui/material';
+import { red } from '@mui/material/colors';
+
+export const theme: ThemeOptions = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
